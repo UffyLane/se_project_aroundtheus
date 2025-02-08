9ede69423036589493c76db108edaca3ecb7783d
Amaterasu <darose07@yahoo.com>
2025-02-07 18:35:56 -0600

    upgrade

---
+++ src/components/UserInfo.js
@@ -0,0 +1,14 @@
+export default class UserInfo {
+  constructor({ profileTitle, profileDescription }, handleProfileEditSubmit) {
+    this._profileTitle = profileTitle;
+    this._profileDescription = profileDescription;
+  }
+
+  getUserInfo() {
+    getUserInfo();
+    this._profileTitle.textContent = profileTitleInput.value;
+    this._profileDescription.textContent = profileDescriptionInput.value;
+  }
+
+  setUserInfo() {}
+}
