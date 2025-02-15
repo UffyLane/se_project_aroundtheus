786c059b8e65a44a1910d369e4eca5d6e131fb41
Amaterasu <darose07@yahoo.com>
2025-02-15 08:22:33 -0600

    upgrade

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -3,7 +3,7 @@ import Popup from "./Popup";
 export default class PopupWithForm extends Popup {
   constructor({ popupSelector, handleFormSubmit }) {
     super({ popupSelector });
-    this._popupElement.querySelector(".modal__form");
+    this._popupForm = this._popupElement.querySelector(".modal__form");
     this._handleFormSubmit = handleFormSubmit;
   }
 
@@ -12,10 +12,19 @@ export default class PopupWithForm extends Popup {
     super.close();
   }
 
-  _getInputValues() {}
+  _getInputValues() {
+    this._inputList = this._popupElement.querySelectorAll(".modal__input");
+    this._formValues = {};
+    this._inputList.forEach((input) => {
+      this._formValues[input.name] = input.value;
+    });
+
+    return this._formValues;
+  }
 
   setEventListeners() {
-    this._popupElement.addEventListener("submit", handleProfileEditSubmit);
-    this._popupElement.addEventListener("submit", handleAddCardFormSubmit);
+    super.setEventListeners();
+
+    this._popupElement.addEventListener("submit", this._submitFunction);
   }
 }
