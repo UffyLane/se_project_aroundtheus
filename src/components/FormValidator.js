33bc6312f96aa7216a64fda699b62cee28632d3f
Amaterasu <darose07@yahoo.com>
2025-03-01 07:51:10 -0600

    upgrade

--- src/components/FormValidator.js
+++ src/components/FormValidator.js
@@ -41,13 +41,13 @@ export default class FormValidator {
     } 
     else {
       this._submitButton.classList.remove(this._inactiveButtonClass);
-      this._submitButton.disabled = false;
+      this._submitButton.disabled = true;
     }
   }
 
   disableButton() {
-    this._submitButton.classList.remove(this._inactiveButtonClass);
-    this._submitButton.disabled = false;
+    this._submitButton.classList.add(this._inactiveButtonClass);
+    this._submitButton.disabled = true;
     return;
   }
 
